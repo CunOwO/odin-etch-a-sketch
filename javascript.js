48f305594044e0ab6aff4a23e306e8d9dd3719a2
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-08-19 13:42:30 +0700

    Update Etch-a-Sketch project:
    - index.html: Added a button for resizing the grid and a new div for the grid.
    - styles.css: Changed background color to pink, added gap between elements, and styled the button.
    - javascript.js: Implemented grid resizing functionality, refactored grid creation and deletion logic.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,43 @@
-const gridContainer = document.querySelector(".container");
+const container =document.querySelector(".container");
+let grid = document.querySelector(".grid");
+const resizeBtn = document.querySelector("#resizeBtn");
+let gridSize;
 
-for (let i = 0; i < 16; i++)
-{
-    const gridColumn = document.createElement("div");
-    gridColumn.setAttribute("style", "display: flex; flex-direction: column; flex: 1;");
-    gridContainer.appendChild(gridColumn);
-    for (let j = 0; j < 16; j++) {
-        const gridCell = document.createElement("div");
-        gridCell.classList.add("pixel");
-        gridCell.setAttribute("style", "border: solid black 0.1px; flex: 1;");
-        gridColumn.appendChild(gridCell);
+createGrid(gridSize);
+
+resizeBtn.addEventListener("click", () => {
+    do {
+        gridSize = parseInt(prompt("Grid size: "));
+    } while (gridSize > 100);
+    
+    deleteGrid();
+    createGrid(gridSize);
+});
+
+function createGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++)
+    {
+        const gridColumn = document.createElement("div");
+        gridColumn.setAttribute("style", "display: flex; flex-direction: column; flex: 1;");
+        grid.appendChild(gridColumn);
+        for (let j = 0; j < gridSize; j++) {
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("pixel");
+            gridCell.setAttribute("style", "border: solid black 0.1px; flex: 1;");
+            gridColumn.appendChild(gridCell);
+        }
     }
-}
-
-const gridCellList = document.querySelectorAll(".pixel");
-gridCellList.forEach((cell) => {
+    const gridCellList = document.querySelectorAll(".pixel");
+    gridCellList.forEach((cell) => {
     cell.addEventListener("mouseenter", () => {
         cell.style.backgroundColor = "black";
     });
 });
+}
+
+function deleteGrid() {
+    container.removeChild(grid);
+    grid = document.createElement("div");
+    grid.classList.add("grid");
+    container.appendChild(grid);
+}
